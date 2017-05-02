72631e68cdf8aab1399f575f8be4606fd0b37207
charltongroves <charltongroves@gmail.com>
2017-05-02 15:27:56 +1000

    added carousel

--- src/js/components/ImageCarousel.js
+++ src/js/components/ImageCarousel.js
@@ -1,17 +1,84 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import Slider from 'react-slick'
+import Easle from '../../img/easle.svg'
+import NextIcon from 'material-ui/svg-icons/image/navigate-next';
+import BeforeIcon from 'material-ui/svg-icons/image/navigate-before';
+import IconButton from 'material-ui/IconButton';
 
-class ImageGallery extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      navOpen: false,
-      currentRoute: props.currentRoute
-    };
-  }
-    render() {
-        return (
-        <div> <p> HEY </p> </div>
-        )
+
+function SampleNextArrow(props) {
+  const {className, onClick} = props
+  const styles = {
+    icon: {
+      width: "48px",
+      height: "48px",
+      color: "rgba(255,255,255,1)"
+    },
+    button: {
+      width: 96,
+      height: 96,
+      padding: 24,
     }
+  }
+  return (
+    <div
+      className={"arrow arrow-next"}
+      onClick={onClick}
+    ><IconButton iconStyle={styles.icon} style={styles.button}><NextIcon/></IconButton></div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const {className, style, onClick} = props
+  const styles = {
+    icon: {
+      width: "48px",
+      height: "48px",
+      color: "rgba(255,255,255,1)"
+    },
+    button: {
+      width: 96,
+      height: 96,
+      padding: 24,
+    }
+  }
+  return (
+    <div
+      className={"arrow arrow-prev"}
+      onClick={onClick}
+    ><IconButton iconStyle={styles.icon} style={styles.button}><BeforeIcon/></IconButton></div>
+  );
+}
+
+export default class ImageCarousel extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      lazyLoad: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      initialSlide: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+    };
+    const styles= {
+      img: {
+        maxWidth: "300px",
+        margin: "0 auto"
+      }
+    }
+    return (
+      <div style={this.props.style}>
+        <h2> Lazy Load</h2>
+        <Slider {...settings}>
+          <div><img style={styles.img} src={Easle} /></div>
+          <div><img style={styles.img} src={Easle} /></div>
+          <div><img style={styles.img} src={Easle} /></div>
+          <div><img style={styles.img} src={Easle} /></div>
+        </Slider>
+      </div>
+    );
+  }
 }
--- src/js/pages/art.js
+++ src/js/pages/art.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import ImageCarousel from '../components/ImageCarousel'
 import '../../css/art.css';
 class Art extends Component {
   render() {
+    const styles = {
+      carouselCont: {
+        width: "900px",
+        margin: "0 auto"
+      }
+    }
     return (
       <div>
+        <ImageCarousel style={styles.carouselCont} />
       </div>
     );
   }
